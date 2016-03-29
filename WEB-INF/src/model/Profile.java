46462ab0632b41f2ee76db09346ffab5e51aeafc
Michael Morris <mcmorris@ualberta.ca>
2016-03-29 13:22:23 -0600

    Update Profile.java

--- WEB-INF/src/model/Profile.java
+++ WEB-INF/src/model/Profile.java
@@ -21,17 +21,17 @@ import session.DBHandler;
  *
  */
 public class Profile {
-  
-  // Add a profile to Profile.
-  public void add(Connection conn, Request request, String user) {
+	
+  	// Add a profile to Profile.
+  	public void add(Connection conn, Request request, String user) {
 	  if (conn == null) return;
 		
 		String user = request.getParameter("USERID");
-    String fName = request.getParameter("FNAME");
-    String lName = request.getParameter("LNAME");
-    String address = request.getParameter("ADDRESS");
-    String email = request.getParameter("EMAIL");
-    String phone = request.getParameter("PHONE");
+		String fName = request.getParameter("FNAME");
+		String lName = request.getParameter("LNAME");
+		String address = request.getParameter("ADDRESS");
+		String email = request.getParameter("EMAIL");
+		String phone = request.getParameter("PHONE");
 		
 		PreparedStatement pstmt = conn.prepareStatement("INSERT INTO persons values(?, ?, ?, ?, ?, ?);");
 		pstmt.setString(1, user);
@@ -41,22 +41,22 @@ public class Profile {
 		pstmt.setString(5, email);
 		pstmt.setString(6, phone);
 		pstmt.executeUpdate();
-  }
+	}
   
-  // Get a person from persons.
-  public ResultSet get(Connection conn, String user) {
-    ResultSet results = null;
-    
-    if (conn != null) {
-      PreparedStatement pstmt = conn.prepareStatement("SELECT * FROM persons WHERE user_name = ?;");
-      pstmt.setString(1, user);
-      
-      results = pstmt.executeQuery();
-    }
-    
-    return results;
-  }
-  
-  // Update and delete group not in assignment specs, and so are not included here.  We had enough concerns to manage as is.
-  
+	// Get a person from persons.
+  	public ResultSet get(Connection conn, String user) {
+		ResultSet results = null;
+		
+		if (conn != null) {
+			PreparedStatement pstmt = conn.prepareStatement("SELECT * FROM persons WHERE user_name = ?;");
+			pstmt.setString(1, user);
+			
+			results = pstmt.executeQuery();
+		}
+		
+		return results;
+	}
+	
+	// Update and delete group not in assignment specs, and so are not included here.  We had enough concerns to manage as is.
+	
 }
