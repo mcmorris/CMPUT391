b1c483e2f5e45228f28c65b642c526d501de3187
Michael Morris <mcmorris@ualberta.ca>
2016-03-29 14:21:44 -0600

    Update User.java

--- WEB-INF/src/model/User.java
+++ WEB-INF/src/model/User.java
@@ -21,31 +21,40 @@ import session.DBHandler;
  *
  */
 public class User {
-    // Add a profile to Profile.
-    public void add(Connection conn, Request request) {
-    if (conn == null) return;
-		
+
+	/*
+	 * Add a profile to Profile.
+	 */
+	public void add(Connection conn, Request request) {
+	    	if (conn == null) return;
+			
 		String user = request.getParameter("USERID");
 		String pwd = request.getParameter("PASSWD");
-    
+		
 		PreparedStatement pstmt = conn.prepareStatement("INSERT INTO users values(?, ?, SYSDATE);");
 		pstmt.setString(1, user);
 		pstmt.setString(2, pwd);
 		pstmt.executeUpdate();
 	}
-  
-	// Get a person from persons.
-  	public ResultSet get(Connection conn, String user) {
+	
+	/*
+	 * Check username/password do not match an existing account.
+	 */
+	public boolean isAvailable(Connection conn, String userName, String passwd) throws Exception, SQLException {
 		ResultSet results = null;
+		boolean available = false;
 		
 		if (conn != null) {
-			PreparedStatement pstmt = conn.prepareStatement("SELECT * FROM persons WHERE user_name = ?;");
+			PreparedStatement pstmt = conn.prepareStatement("COUNT(*) FROM users WHERE user_name = ?;");
 			pstmt.setString(1, user);
 			
 			results = pstmt.executeQuery();
+			if (results != null && results.getInt(1) == 0) {
+				available = true;
+			}
 		}
 		
-		return results;
+		return available;
 	}
 	
 	// Update and delete group not in assignment specs, and so are not included here.  We had enough concerns to manage as is.
