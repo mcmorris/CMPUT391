4968522eacec1c41240a2cec5c8287370e9dab85
Michael Morris <mcmorris@ualberta.ca>
2016-03-29 22:15:14 -0600

    Create getRankedPics.sql

---
+++ sql/getRankedPics.sql
@@ -0,0 +1,23 @@
+  		SELECT ALL IMAGES FROM IMAGES
+		WHERE USER IS ABLE TO ALLOWED TO ACCESS
+		
+		FROM THIS
+		SELECT FIVE HIGHEST PICTURES
+
+		Rank(photo_id) = SELECT
+		
+		
+		Rank(photo_id) = 6*frequency(subject) + 3*frequency(place) + frequency(description)
+  		
+  		SELECT score(1), itemName, description FROM item WHERE contains(description, ?, 1) > 0 order by score(1) desc
+  		
+  		SELECT SCORE(1), title FROM photos
+		WHERE CONTAINS(subject, 'dog', 1) > 0
+		ORDER BY SCORE(10) DESC;
+
+  		
+  		SELECT * FROM (SELECT photo_id, 
+  			rank() over (order by )
+  			
+  			)
+  			WHERE rnk <= 5
